3d59772921425d8a250353dc9b1ca19c5796d5e8
dante ccarhuas <ccarhuas.dante@gmail.com>
2016-06-26 23:58:42 -0500

    Cambio Prueba

--- app/src/main/java/pe/com/pandero/panderoandroid/activity_login.java
+++ app/src/main/java/pe/com/pandero/panderoandroid/activity_login.java
@@ -1,17 +1,15 @@
 package pe.com.pandero.panderoandroid;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
-public class LoginActivity extends AppCompatActivity implements View.OnClickListener{
+public class activity_login extends Activity implements View.OnClickListener{
 
     Button btn_login;
 
@@ -62,7 +60,8 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
     public void onClick(View v) {
         switch (v.getId()){
             case R.id.btn_login:
-                Toast.makeText(LoginActivity.this,"HOla Que tal",Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(activity_login.this, activity_menu.class);
+                startActivity(intent);
                 break;
         }
     }
