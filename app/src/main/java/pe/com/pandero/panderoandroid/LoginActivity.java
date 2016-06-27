53c5d043ec6e8b6adf3b8bd9c2f4357702724e36
dante ccarhuas <ccarhuas.dante@gmail.com>
2016-06-26 22:38:00 -0500

    Cambio Prueba

--- app/src/main/java/pe/com/pandero/panderoandroid/LoginActivity.java
+++ app/src/main/java/pe/com/pandero/panderoandroid/LoginActivity.java
@@ -8,8 +8,11 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
 
 public class LoginActivity extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
