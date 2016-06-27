036687c49ee91ad2cd95e10cf68aaf9f0c4254cd
dante ccarhuas <ccarhuas.dante@gmail.com>
2016-06-26 22:13:48 -0500

    Cambio Prueba

---
+++ app/src/main/java/pe/com/pandero/panderoandroid/activity_menu.java
@@ -0,0 +1,13 @@
+package pe.com.pandero.panderoandroid;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class activity_menu extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu);
+    }
+}
